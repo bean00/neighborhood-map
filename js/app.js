50044fb04ae584d2b5761d5fe5935178236a1f0e
bean00 <jcbuilder40@hotmail.com>
2018-07-25 13:35:09 -0700

    Display locations in a list
    - Can click on a location to open its infoWindow

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-function AppViewModel() {
+const locations = [
+  {id: 0, title: 'Apple Campus', location: {lat: 37.33182, lng: -122.03118}},
+  {id: 1, title: 'Whole Foods', location: {lat: 37.323577, lng: -122.039895}},
+  {id: 2, title: '85 Degrees C', location: {lat: 37.303966, lng: -122.032824}}
+];
 
+var viewModel = {
+  locations: ko.observableArray(locations)
 }
 
-ko.applyBindings(new AppViewModel());
+ko.applyBindings(viewModel);
--- js/map.js
+++ js/map.js
@@ -1,20 +1,15 @@
-const locations = [
-  {title: 'Apple Campus', location: {lat: 37.33182, lng: -122.03118}},
-  {title: 'Whole Foods', location: {lat: 37.323577, lng: -122.039895}},
-  {title: '85 Degrees C', location: {lat: 37.303966, lng: -122.032824}}
-];
-
 const cupertino = {lat: 37.322998, lng: -122.032182};
 
 let map;
 let markers = [];
+let infoWindow;
 const mapCenter = cupertino;
 const mapZoom = 13;
 
 function initMap() {
   map = createMap(mapCenter, mapZoom);
 
-  markers = createMarkers(locations);
+  markers = createMarkers(viewModel.locations());
 
   google.maps.event.addListenerOnce(map, 'idle', function() {
     setMapBoundaries(map, markers);
@@ -32,12 +27,12 @@ function createMap(center, zoom) {
 
 function createMarkers(locations) {
   let markers = [];
-  const infoWindow = new google.maps.InfoWindow();
+  infoWindow = new google.maps.InfoWindow();
 
   locations.forEach(function(location) {
     let marker = createMarker(map, location);
 
-    // Open an infowindow at each marker
+    // Open an infoWindow at each marker
     marker.addListener('click', function() {
       populateInfoWindow(this, infoWindow);
     });
@@ -50,6 +45,7 @@ function createMarkers(locations) {
 
 function createMarker(map, location) {
   const marker = new google.maps.Marker({
+    id: location.id,
     map: map,
     position: location.location,
     title: location.title
@@ -68,30 +64,34 @@ function setMapBoundaries(map, markers) {
   map.fitBounds(bounds);
 }
 
-function populateInfoWindow(marker, infowindow) {
-  const markersInfoWindowIsOpen = infowindow.marker == marker;
+function populateInfoWindow(marker, infoWindow) {
+  const markersInfoWindowIsOpen = infoWindow.marker == marker;
 
   if (!markersInfoWindowIsOpen) {
-    openInfoWindow(infowindow, map, marker);
+    openInfoWindow(marker);
 
-    // Clear the marker property if the infowindow is closed
-    infowindow.addListener('closeclick', function() {
-      infowindow.setMarker = null;
+    // Clear the marker property if the infoWindow is closed
+    infoWindow.addListener('closeclick', function() {
+      infoWindow.setMarker = null;
     });
   }
 }
 
-function openInfoWindow(infowindow, map, marker) {
-  infowindow.marker = marker;
+function openInfoWindow(marker) {
+  infoWindow.marker = marker;
 
   const content = buildInfoWindowContent(marker);
-  infowindow.setContent(content);
+  infoWindow.setContent(content);
 
-  infowindow.open(map, marker);
+  infoWindow.open(map, marker);
 }
 
 function buildInfoWindowContent(marker) {
   const title = marker.title;
 
   return '<div>' + title + '</div>';
+}
+
+function openWindowFromList(locationId) {
+  openInfoWindow(markers[locationId]);
 }
